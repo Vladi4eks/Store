e54c4c7eda427a928363ab9e5a0e70e124e28076
Vladi4ek <jeryygood@gmail.com>
2022-04-08 15:12:42 +0300

    Add interface for payment

---
+++ src/base/Payment.java
@@ -0,0 +1,13 @@
+package base;
+
+/**
+ * @author Kalashnikov Vladislav
+ * @version 1.0.0
+ */
+
+public interface Payment {
+
+    double calcPayment(int quantity, double price);
+
+    double calcPayment(double payment, double discount);
+}
