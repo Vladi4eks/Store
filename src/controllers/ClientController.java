e729eb6ebea3075232bb0f3c116e5172f4c58dea
Vladi4ek <jeryygood@gmail.com>
2022-04-08 19:17:33 +0300

    Added controller for client

---
+++ src/controllers/ClientController.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import models.Client;
+import utils.Rounder;
+import views.ClientView;
+
+/**
+ * @author Kalashnikov Vladislav
+ * @version 1.0.0
+ */
+
+public class ClientController {
+
+    Client model;
+    ClientView view;
+
+    public ClientController(Client model, ClientView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void runApp() {
+
+        view.getInputs();
+
+        String name = model.getName();
+
+        double paymentInitial = model.calcPayment(model.getQuantity(), model.getPrice());
+        String paymentInitialRounded = Rounder.roundValue(paymentInitial);
+
+        double discount = model.calcDiscount(paymentInitial);
+        String discountRounded = Rounder.roundValue(discount);
+
+        String paymentRounded = Rounder.roundValue(model.calcPayment(paymentInitial, discount));
+
+        String output = "------------------------------\n" +
+                "Имя клиента: " + name + "\n" +
+                "Сума покупки (грн.): " + paymentInitialRounded + "\n" +
+                "Сумма скидки (грн.): " + discountRounded + "\n" +
+                "К оплате (грн.): " + paymentRounded;
+
+        view.getOutput(output);
+    }
+}
