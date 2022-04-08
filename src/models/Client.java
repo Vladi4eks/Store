c4d3ae3b043db9913e4239b6d0624efe47f37cf8
Vladi4ek <jeryygood@gmail.com>
2022-04-08 16:12:50 +0300

    Added model for client

---
+++ src/models/Client.java
@@ -0,0 +1,56 @@
+package models;
+
+import base.Discount;
+import base.Payment;
+
+/**
+ * @author Kalashnikov Vladislav
+ * @version 1.0.0
+ */
+
+public class Client implements Payment, Discount {
+
+    private String name;
+    private int quantity;
+    private double price;
+    private final static int DISCOUNT_RATE = 15;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public double calcPayment(int quantity, double price) {
+        return quantity * price;
+    }
+
+    @Override
+    public double calcPayment(double payment, double discount) {
+        return payment - discount;
+    }
+
+    @Override
+    public double calcDiscount(double payment) {
+        return payment * DISCOUNT_RATE / 100;
+    }
+}
