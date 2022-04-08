20ce7c5e98a57cde5c105ce5680e03ddef3c9792
Vladi4ek <jeryygood@gmail.com>
2022-04-08 18:58:50 +0300

    Changed App

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import controllers.ClientController;
+import models.Client;
+import views.ClientView;
+
 /**
  * @author Kalashnikov Vladislav
  * @version 1.0.0
@@ -7,6 +11,10 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hi! This is my new application.");
+        Client model = new Client();
+        ClientView view = new ClientView(model);
+        ClientController controller = new ClientController(model, view);
+
+        controller.runApp();
     }
 }
